0fb4ce9140d2ab40a834b600519967108b508a26
Yimeng Zhang <zym1010@gmail.com>
2014-03-03 15:20:05 -0500

    add some parameters into dbnsetup

--- DBN/dbnsetup.m
+++ DBN/dbnsetup.m
@@ -2,9 +2,19 @@ function dbn = dbnsetup(dbn, x, opts)
     n = size(x, 2);
     dbn.sizes = [n, dbn.sizes];
 
+    if isfield(dbn,'types') % assign each layer a type
+        assert(numel(dbn.types) == numel(dbn.sizes),'no type or all specified');
+    else % we create it.
+        dbn.types = repmat({'binary'},1,numel(dbn.sizes));
+    end
+    
     for u = 1 : numel(dbn.sizes) - 1
         dbn.rbm{u}.alpha    = opts.alpha;
         dbn.rbm{u}.momentum = opts.momentum;
+        
+        if isfield(opts,'weightPenaltyL2') % add weight penalty, vectorized version possible in the future
+            dbn.rbm{u}.weightPenaltyL2 = opts.weightPenaltyL2;
+        end
 
         dbn.rbm{u}.W  = zeros(dbn.sizes(u + 1), dbn.sizes(u));
         dbn.rbm{u}.vW = zeros(dbn.sizes(u + 1), dbn.sizes(u));
